bfd90c2a0ce65d0d24e07861324e0983578f7454
Luan Silva <luansilvatec@unifesspa.edu.br>
2020-06-01 02:14:38 -0300

    Updates

---
+++ precache-manifest.1633e90b7a25443112d0305c9e313305.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532c876e31e7089f1f48de6cd6650e8e",
+    "url": "/my-page/index.html"
+  },
+  {
+    "revision": "0afcf3f9426e7e13674d",
+    "url": "/my-page/static/css/main.7ed46ad5.chunk.css"
+  },
+  {
+    "revision": "2d6e0955328aa5fecadc",
+    "url": "/my-page/static/js/2.c782c7f4.chunk.js"
+  },
+  {
+    "revision": "402769e0fde7682650babf6b5720df16",
+    "url": "/my-page/static/js/2.c782c7f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0afcf3f9426e7e13674d",
+    "url": "/my-page/static/js/main.71ca4878.chunk.js"
+  },
+  {
+    "revision": "c58ba8ab80834e69256f",
+    "url": "/my-page/static/js/runtime-main.14668b25.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/my-page/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/my-page/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/my-page/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/my-page/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/my-page/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "aa6825f5cde5f84ad8a55f93dd7afdc6",
+    "url": "/my-page/static/media/luan.aa6825f5.JPEG"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/my-page/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/my-page/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/my-page/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/my-page/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/my-page/static/media/nucleo-icons.f82ec6ba.ttf"
+  }
+]);
